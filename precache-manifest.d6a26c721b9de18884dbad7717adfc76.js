3254e0123b58be1a02a3cf2fa37eb70188a2d0a4
Renan Brayner <renanbrayner@gmail.com>
2020-02-18 10:41:11 -0300

    Updates

---
+++ precache-manifest.d6a26c721b9de18884dbad7717adfc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d60fb6b76029c24edfa4ff725878e0",
+    "url": "/gitsearch/index.html"
+  },
+  {
+    "revision": "8009a9ca08c821e2abe2",
+    "url": "/gitsearch/static/css/main.fe7ae2ab.chunk.css"
+  },
+  {
+    "revision": "161e1afcaccbd04f9e50",
+    "url": "/gitsearch/static/js/2.e65669ae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gitsearch/static/js/2.e65669ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8009a9ca08c821e2abe2",
+    "url": "/gitsearch/static/js/main.e9e98d25.chunk.js"
+  },
+  {
+    "revision": "d125c3984e8fed087da9",
+    "url": "/gitsearch/static/js/runtime-main.5b6dde11.js"
+  }
+]);
