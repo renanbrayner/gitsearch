ce739a574602c00cb83c38747172606e9db3d5d5
Renan Brayner <renanbrayner@gmail.com>
2020-02-18 10:37:51 -0300

    favicon e titulo da pagina corrigido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import ReactDOM from "react-dom";
 
 /* Components */
